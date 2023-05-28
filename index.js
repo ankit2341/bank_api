11c8996ec3879e280a6432ebf561b8a79ec236de
ankit2341 <ankitpatil2341@gmail.com>
2023-05-28 13:38:47 +0530

    cors policy

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   next();
 });
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
 
 app.use("/users", userRouter);
 app.use("/accounts", accountRouter);
