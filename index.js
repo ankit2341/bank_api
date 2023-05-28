6cbdf21e1099a80571cb0df95f7c3851544aa1fe
ankit2341 <ankitpatil2341@gmail.com>
2023-05-28 10:43:42 +0530

    deployed

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
-const {connection}=require("./config/database");
-const cors=require("cors");
-const express=require("express");
+const { connection } = require("./config/database");
+const cors = require("cors");
+const express = require("express");
 const { accountRouter } = require("./routes/Accounts.route");
 const { userRouter } = require("./routes/Users.route");
 
-const app=express();
+const app = express();
 app.use(express.json());
-app.use(cors({origin:"*"}));
+app.use(cors({ origin: "*" }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 
-app.use("/users",userRouter);
-app.use("/accounts",accountRouter);
+app.use("/users", userRouter);
+app.use("/accounts", accountRouter);
 
+app.get("/", async (req, res) => {
+  try {
+    res.status(200).send("welocme to bank database");
+  } catch (err) {
+    res.status(404).send({ msg: "error connecting to api" });
+  }
+});
 
-app.get("/",async(req,res)=>{
-    try{
-       res.status(200).send("welocme to bank database");
-    }
-    catch(err){
-        res.status(404).send({"msg":"error connecting to api"})
-    }
-})
-
-app.listen(4500,async(req,res)=>{
-    try{
-        await connection;
-        console.log("connected to db")
-    }
-    catch(err){
-        console.log(err)
-    }
-    console.log("running server")
-})
+app.listen(4500, async (req, res) => {
+  try {
+    await connection;
+    console.log("connected to db");
+  } catch (err) {
+    console.log(err);
+  }
+  console.log("running server");
+});
